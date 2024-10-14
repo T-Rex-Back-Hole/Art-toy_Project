54d0fcddd62c2285eaa341925755574758e20d5d
karnpun9 <chayada.pun9@gmail.com>
2024-10-14 13:30:24 +0700

    add font family poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Poppins']
+        
+      }
+    },
   },
   plugins: [],
 };
