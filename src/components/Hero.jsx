8bb7ef7c75b4cdb2817e2f14081b584d4a3509cc
iceparinwat <iceparinwat@IceParinwat>
2024-10-29 11:33:04 +0700

    edit page react

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-    return (
-        <>
-            <section id="slider-container">
-                <div id="slider-wrap" className="relative max-w-*">
-                    <div id="slider" className="flex overflow-x-auto snap-x snap-mandatory scroll-smooth">
-                        {/* <img id="slider-1" src="./images/banner1.png" alt="" className="grow shink-0 basis-full snap-start object-cover "/> */}
-                        <img id="slider-2" src="./images/banner2.png" alt="" className="grow shink-0 basis-full snap-start object-cover"/>
-                    </div>
-                </div>
-            </section>
-            
-        </>
-    )
-}
+  return (
+    <>
+      <section id="slider-container">
+        <div id="slider-wrap" className="relative max-w-*">
+          <div
+            id="slider"
+            className="flex overflow-x-auto snap-x snap-mandatory scroll-smooth"
+          >
+            {/* <img id="slider-1" src="./images/banner1.png" alt="" className="grow shink-0 basis-full snap-start object-cover "/> */}
+            <img
+              id="slider-2"
+              src="./images/banner2.png"
+              alt=""
+              className="grow shink-0 basis-full snap-start object-cover"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
